649719f021f310d773bfd89f5292e4eeba346f3c
David Dahlgren <ddaviddahlgren@gmail.com>
2026-09-10 10:21:36 +0200

    tidy code

--- src/app/jobs/page.jsx
+++ src/app/jobs/page.jsx
@@ -1,22 +1,21 @@
-import { getStoryblokApi } from "@/lib/storyblok"
-import { StoryblokServerComponent } from "@storyblok/react/rsc"
+import { getStoryblokApi } from "@/lib/storyblok";
+import { StoryblokServerComponent } from "@storyblok/react/rsc";
 
 export default async function JobsPage({ searchParams }) {
-    const params = await searchParams
-    const { data } = await fetchData()
+  const params = await searchParams;
+  const { data } = await fetchData();
 
-    return (
-        <div className="jobs-page">
-            <StoryblokServerComponent
-                blok={data.story.content}
-                department={params.department}
-                q={params.q}
-            />
-        </div>
-    )
+  return (
+    <div className="jobs-page">
+      <StoryblokServerComponent
+        blok={data.story.content}
+        searchParams={params}
+      />
+    </div>
+  );
 }
 
 async function fetchData() {
-    const storyblokApi = getStoryblokApi()
-    return await storyblokApi.get("cdn/stories/jobs", { version: "draft" })
+  const storyblokApi = getStoryblokApi();
+  return await storyblokApi.get("cdn/stories/jobs", { version: "draft" });
 }
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,10 +1,10 @@
 import { storyblokEditable, StoryblokServerComponent } from "@storyblok/react/rsc";
 
-export default function Page({ blok }) {
+export default function Page({ blok, ...rest }) {
   return (
     <main {...storyblokEditable(blok)}>
       {blok.body?.map((nestedBlok) => (
-        <StoryblokServerComponent blok={nestedBlok} key={nestedBlok._uid} />
+        <StoryblokServerComponent blok={nestedBlok} key={nestedBlok._uid} {...rest}/>
       ))}
     </main>
   );
